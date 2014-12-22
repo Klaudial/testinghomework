e3c9e5bf8d9dfd7edbcbcd0362165419f88145de
Claudia <claudialclarke@aol.com>
2014-12-22 21:29:56 +0000

    Removed redundant test

--- test_montecarloalgorithm.py
+++ test_montecarloalgorithm.py
@@ -14,10 +14,9 @@ def testiter():
     with assert_raises(ValueError) as exception: MonteCarloAlgorithm(iternumb=-1)
     with assert_raises(TypeError) as exception: MonteCarloAlgorithm(iternumb= 0.7)
     
-#from filefortest import newtemperature
+
    
 #def testtemp(): #put this back?
- #   assert_not_equal(newtemperaturevalue(customtemp==False), 300)  
+    #from filefortest import Newtemperature
+    #assert_not_equal(newtemperaturevalue(customtemp==False), 300)  
     
-#def functionnumbertest()
-   # with assert_raises(TypeError) as exception: MonteCarloAlgorithm(functionnumber=1.5)
